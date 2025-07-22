d933de633586c0f86a1b6f4904960a8d1629928d
Zach Gibbs <zacharywgibbs@gmail.com>
2025-07-21 20:46:25 -0500

    commenting out chatbot route.

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,61 +1,61 @@
-import dotenv from "dotenv";
-dotenv.config({ path: ".env.local" });
-// Configure dotenv before other imports
+// import dotenv from "dotenv";
+// dotenv.config({ path: ".env.local" });
+// // Configure dotenv before other imports
 
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import { DocumentInterface } from "@langchain/core/documents";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import {
-  getEmbeddingsCollection,
-  getVectorStore,
-} from "../src/app/lib/astradb";
+// import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+// import { TextLoader } from "langchain/document_loaders/fs/text";
+// import { DocumentInterface } from "@langchain/core/documents";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+// import {
+//   getEmbeddingsCollection,
+//   getVectorStore,
+// } from "../src/app/lib/astradb";
 
-async function generateEmbeddings() {
-  const vectorStore = await getVectorStore();
+// async function generateEmbeddings() {
+//   const vectorStore = await getVectorStore();
 
-  (await getEmbeddingsCollection()).deleteMany({});
+//   (await getEmbeddingsCollection()).deleteMany({});
 
-  const loader = new DirectoryLoader(
-    "src/app/",
-    {
-      ".tsx": (path) => new TextLoader(path),
-      ".ts": (path) => new TextLoader(path),
-    },
-    true,
-  );
+//   const loader = new DirectoryLoader(
+//     "src/app/",
+//     {
+//       ".tsx": (path) => new TextLoader(path),
+//       ".ts": (path) => new TextLoader(path),
+//     },
+//     true,
+//   );
 
-  const docs = (await loader.load())
-    .filter(
-      (doc) =>
-        doc.metadata.source.endsWith("page.tsx") ||
-        doc.metadata.source.endsWith("projectData.ts") ||
-        doc.metadata.source.endsWith("resume.ts"),
-    )
-    .map((doc): DocumentInterface => {
-      const url =
-        doc.metadata.source
-          .replace(/\\/g, "/")
-          .split("src/app")[1]
-          .split("/page.")[0] || "/";
+//   const docs = (await loader.load())
+//     .filter(
+//       (doc) =>
+//         doc.metadata.source.endsWith("page.tsx") ||
+//         doc.metadata.source.endsWith("projectData.ts") ||
+//         doc.metadata.source.endsWith("resume.ts"),
+//     )
+//     .map((doc): DocumentInterface => {
+//       const url =
+//         doc.metadata.source
+//           .replace(/\\/g, "/")
+//           .split("src/app")[1]
+//           .split("/page.")[0] || "/";
 
-      const pageContentTrimmed = doc.pageContent
-        .replace(/^import.*$/gm, "") // Remove all import statements
-        .replace(/ className=(["']).*?\1| className={.*?}/g, "") // Remove all className props
-        .replace(/^\s*[\r]/gm, "") // remove empty lines
-        .trim();
+//       const pageContentTrimmed = doc.pageContent
+//         .replace(/^import.*$/gm, "") // Remove all import statements
+//         .replace(/ className=(["']).*?\1| className={.*?}/g, "") // Remove all className props
+//         .replace(/^\s*[\r]/gm, "") // remove empty lines
+//         .trim();
 
-      return {
-        pageContent: pageContentTrimmed,
-        metadata: { url },
-      };
-    });
+//       return {
+//         pageContent: pageContentTrimmed,
+//         metadata: { url },
+//       };
+//     });
 
-  const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
+//   const splitter = RecursiveCharacterTextSplitter.fromLanguage("html");
 
-  const splitDocs = await splitter.splitDocuments(docs);
+//   const splitDocs = await splitter.splitDocuments(docs);
 
-  await vectorStore.addDocuments(splitDocs);
-}
+//   await vectorStore.addDocuments(splitDocs);
+// }
 
-generateEmbeddings();
+// generateEmbeddings();
